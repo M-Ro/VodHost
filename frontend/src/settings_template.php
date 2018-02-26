f19e94c1db17362e5f3072a68efbd743bd19499d
Michael Rodenhurst <mrodenhurst1@gmail.com>
2018-02-26 16:50:05 +0000

    Fix indentation

--- frontend/src/settings_template.php
+++ frontend/src/settings_template.php
@@ -1,32 +1,32 @@
 <?php
 
 $config = [
-	// PHP Settings
-	'displayErrorDetails' => true,
-	'addContentLengthHeader' => false,
+    // PHP Settings
+    'displayErrorDetails' => true,
+    'addContentLengthHeader' => false,
 
-	// Path to the SQLite database file
-	'SQLiteFilePath' => 'db/sqlite.db',
+    // Path to the SQLite database file
+    'SQLiteFilePath' => 'db/sqlite.db',
 
-	// Directory where chunked uploads will be held
-	'temp_directory' => __DIR__ . '/temp',
+    // Directory where chunked uploads will be held
+    'temp_directory' => __DIR__ . '/temp',
 
-	// Directory where videos will be delivered from
-	'upload_directory' => __DIR__ . '/processing',
+    // Directory where videos will be delivered from
+    'upload_directory' => __DIR__ . '/processing',
 
-	// Job Queue -- RabbitMQ Configuration
-	'jobq_server' => 'localhost',
-	'jobq_port' => 5672,
-	'jobq_user' => 'guest',
-	'jobq_pass' => 'guest',
+    // Job Queue -- RabbitMQ Configuration
+    'jobq_server' => 'localhost',
+    'jobq_port' => 5672,
+    'jobq_user' => 'guest',
+    'jobq_pass' => 'guest',
 
-	// Log Server -- Redis Configuration
-	'log_server' => 'localhost',
-	'log_port' => 6379,
-	'log_pass' => 'password',
+    // Log Server -- Redis Configuration
+    'log_server' => 'localhost',
+    'log_port' => 6379,
+    'log_pass' => 'password',
 
-	// Database connection
-	'db_connection' => [
+    // Database connection
+    'db_connection' => [
         'driver'   => 'pdo_mysql',
         'host'     => 'localhost',
         'dbname'   => 'vodhost',
