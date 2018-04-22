b51ff0a3ba43522f9b8d502119c3e558f8d989d9
Michael Rodenhurst <mrodenhurst1@gmail.com>
2018-04-22 16:44:07 +0100

    Add events for when a broadcast is uploaded or processed. Add associated queue jobs for processing uploaded videos.

---
+++ app/Http/Controllers/BroadcastController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Broadcast as Broadcast;
+
+class BroadcastController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Return recently uploaded videos
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function recent()
+    {
+        $broadcasts = Broadcast::where('public', true)
+                    ->orderBy('created_at', 'desc')
+                    ->take(50)
+                    ->get();
+
+        $json = $broadcasts->toJson();
+
+        return response()->json($json);
+    }
+}
